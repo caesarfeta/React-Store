3b596cd1eb19faa8549857741d4c2732794b7693
caesarfeta <adamtavares@gmail.com>
2020-01-13 12:31:44 -0500

    layout play

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {useSelector,useDispatch} from 'react-redux';
 import _ from 'lodash';
 import {FaTimesCircle,FaTimes,FaPlus,FaShoppingCart,FaCartPlus} from 'react-icons/fa';
 import {addToCart,rmFromCart,itmN,itmNValid} from './actions';
+import { Container, Row, Col, Jumbotron, Alert, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 function Product(p){
   const cart = useSelector(state=>state.cart);
@@ -86,29 +87,44 @@ function List(){
   </div>
 }
 function Header(){
-  return <h1>LAPLASTX</h1>
+  const about="Welcome to LAPLASTX! We are your source for inspired 3D printable figures and toys! Check out our latest offerings below!"
+  return <Alert variant="primary">
+    <h1>LAPLASTX</h1>
+    <ReactMarkdown source={about} />
+  </Alert>
 }
 function Prod(p){
-  return <div className="prod">
-    <h2>{p.itm.name}</h2>
-    <img src={p.itm.splash}/>
-    <img src={p.itm.logo}/>
-    <img src={p.itm.figure}/>
-    <ReactMarkdown source={p.itm.copy} />
-  </div>
+  const cart = useSelector(state=>state.cart);
+  const dispatch = useDispatch();
+  const cartItm = _.find(cart.itms, function(o){ return o[0]===p.itm });
+  const disable = !!cartItm && cartItm[0].max_quant != null && cartItm[1] >= cartItm[0].max_quant;
+  return <Jumbotron>
+    <Row>
+      <Col><img src={p.itm.logo}/></Col>
+    </Row>
+    <Row>
+      <Col><img src={p.itm.splash}/></Col>
+    </Row>
+    <Row>
+      <Col>
+        <ReactMarkdown source={p.itm.copy} />
+        <button disabled={ disable } onClick={()=>dispatch(addToCart(p.itm))}>Add <FaCartPlus /></button>
+      </Col>
+      <Col><img className="fig" src={p.itm.figure}/></Col>
+    </Row>
+  </Jumbotron>
 }
 function Browse(){
   const prods = _.map( useSelector(state=>state.products),function(itm){
     return <Prod itm={itm} key={itm.id}/>
   })
-  return prods
+  return <Container>{prods}</Container>
 }
 function App() {
   return (
     <div id="app" className="App">
         <Header />
         <Browse />
-        <List />
         <Cart />
     </div>
   );
