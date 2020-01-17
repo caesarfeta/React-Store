588e569f5a64cadcdcc5153886afe7c0597b45e8
caesarfeta <adamtavares@gmail.com>
2020-01-17 13:08:32 -0500

    more auth experimentation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import productReducer from './products';
 import cartReducer from './cart';
+import authReducer from './auth';
+import errorReducer from './error';
 import {combineReducers} from 'redux';
 const allReducers = combineReducers({
   products: productReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  auth: authReducer,
+  error: errorReducer
 })
 export default allReducers
